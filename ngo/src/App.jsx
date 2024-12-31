3163d5ce221b467089e42ee3021a57cbfc4e7743
01Ranjan <ranjankumar459688@gmail.com>
2024-12-31 19:59:34 +0530

    test-by-ranjan

--- ngo/src/App.jsx
+++ ngo/src/App.jsx
@@ -27,7 +27,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        hi it is ritsd
+        hi it is rits
+        it is ranjan
       </p>
     </>
   )
