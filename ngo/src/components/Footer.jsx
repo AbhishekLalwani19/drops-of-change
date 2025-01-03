37d47380f26b790a146afe8195cb8f65b8205fc3
AbhishekLalwani19 <abhisheklalwani2005@gmail.com>
2025-01-03 18:26:11 +0530

    head foot

---
+++ ngo/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../css"
+
+export const Footer = ()=>{
+    return(
+        <>
+            <div className="container">
+
+            </div>
+        </>
+    )
+};
---
+++ ngo/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../css"
+
+export const Header = () =>{
+    return(
+        <>
+            <div className="container" id = "main-box">
+                    
+            </div>
+        </>
+    )
+}
