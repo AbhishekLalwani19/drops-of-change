ec36abfb5cf9bc2d5940b0ef328a1222f1af4a73
Ritamsundarsandhaki <lttritam@gmail.com>
2025-01-03 21:41:49 +0530

    testcomit

--- ngo/src/pages/aboutus.jsx
+++ ngo/src/pages/aboutus.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import '../css/aboutus.css'
-import herodata from '../api/aboutus.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
-function Aboutus() {
-  const [herosetionData, setHerosetionData] = useState([]);
+function Aboutus(){
+    const [herosetionData,setHerosetionData] = useState([]);
 
     const CalltheApi = async ()=>{
         try{
           let  res  = await fetch('/api/aboutus.json');
           let heroSetionApiData = await res.json();
             console.log(heroSetionApiData);
+        setHerosetionData(heroSetionApiData)
         }
         catch(err){
             console.log("error gentrt",err);
@@ -21,13 +20,14 @@ function Aboutus() {
     };
 useEffect(()=>{
     CalltheApi();
-  }, []);
-  return (
+},[])
+return(
     <>
-      <div className="herosetion">
+    <div className="herosetion">
         <div className="herosetionPart1">
             <div className="herosetionTagline">
-                <h1>Sowing Seeds of Change, One Drop at a Time.</h1>
+                {/* <h1>Sowing Seeds of Change, One Drop at a Time.</h1> */}
+                <h1>{herosetionData.length==0?("hi"):herosetionData[0].tagline}</h1>
             </div>
             <div className="herosetionDescription">
                 <h3>
@@ -37,13 +37,10 @@ useEffect(()=>{
             </div>
         </div>
         <div className="herosetionPart2">
-          <img
-            src="https://dropsofchange.in/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-12-at-14.54.33.jpg"
-            alt=""
-          />
+            <img src="https://dropsofchange.in/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-12-at-14.54.33.jpg" alt="" />
         </div>
-      </div>
+    </div>
     </>
-  );
+)
 }
-export default Aboutus;
+export default Aboutus
