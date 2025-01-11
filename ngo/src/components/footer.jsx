5f98f028df1f78be0398b9eac498127eff7736f7
AbhishekLalwani19 <abhisheklalwani2005@gmail.com>
2025-01-11 17:39:16 +0530

    footer

--- ngo/src/components/footer.jsx
+++ ngo/src/components/footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-
+import "../css/footer.css"
 
 export const ABCD = ()=>{
     return(
         <>
         {/* this is the upper part of the footer which will stay contant in every website  */}
            <div className="container" id='footer-partition-one'>
-                
+                <h2>GIVE US A HAND</h2>
+                <h1>SUPPORT US AND CHANGE THE COURSE OF A </h1>
+                <h1>CHILD'S LIFE TODAY!</h1>
             </div>
         {/* this is the second part of the footer */}
-        <div className="container" id='footer-partition-two'></div>
+        <div className="container" id='footer-partition-two'>
 
-
-
+        </div>
         </>
     )
 }
