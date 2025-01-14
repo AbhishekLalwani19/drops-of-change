7f7de7e36676407481622df4a8224f88186df94b
AbhishekLalwani19 <abhisheklalwani2005@gmail.com>
2025-01-14 18:26:06 +0530

    fethcrow

--- ngo/src/components/footer.jsx
+++ ngo/src/components/footer.jsx
@@ -6,9 +6,9 @@ export const Footer = ()=>{
         <>
         {/* this is the upper part of the footer which will stay contant in every website  */}
            <div className="container" id='footer-partition-one'>
-                <h2>GIVE US A HAND</h2>
-                <h1>SUPPORT US AND CHANGE THE COURSE OF A </h1>
-                <h1>CHILD'S LIFE TODAY!</h1>
+                <h2 id='text1-footh2'>GIVE US A HAND</h2>
+                <h1 id='text2-footh1'>SUPPORT US AND CHANGE THE COURSE OF A </h1>
+                <h1 id='text3-footh1'>CHILD'S LIFE TODAY!</h1>
             </div>
         {/* this is the second part of the footer */}
         <div className="container" id='footer-partition-two'>
